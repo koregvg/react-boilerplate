2c3ce59180577100dbc18cfce68a39882d158f68
王派 <wangpai@mybook.com>
2021-03-25 17:47:16 +0800

    up

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -2,37 +2,88 @@ import React, {useState, useEffect, useCallback} from 'react';
 import './about.scss';
 import Layout from '../layout/layout';
 import axios from "axios";
+import {Table, Tag, Space, Input} from 'antd';
+
+const { Search } = Input;
 
 const About = props => {
-    let [state, updateState] = useState({
-        id: 0,
-        data: {
-            a: 0
-        }
-    });
+    let [id, changeId] = useState(props.id || 1)
+    let [data, updateData] = useState([]);
+
+    let columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: text => <a>{text}</a>,
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address',
+        },
+        {
+            title: 'Tags',
+            key: 'tags',
+            dataIndex: 'tags',
+            render: tags => (
+                <>
+                    {tags.map(tag => {
+                        let color = tag.length > 5 ? 'geekblue' : 'green';
+                        if (tag === 'loser') {
+                            color = 'volcano';
+                        }
+                        return (
+                            <Tag color={color} key={tag}>
+                                {tag.toUpperCase()}
+                            </Tag>
+                        );
+                    })}
+                </>
+            ),
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (text, record) => (
+                <Space size="middle">
+                    <a>Invite {record.name}</a>
+                    <a>Delete</a>
+                </Space>
+            ),
+        },
+    ];
 
     const getList = useCallback(() => {
-        axios.get(`/v2/getList/id=${state.id}`, {
-            params: {
-                a: 1
-            }
+        axios.get(`/v2/getList/id=${id}`, {
         }).then(res => {
-            updateState(res.data.data)
+            updateData(res.data.data)
+            console.log('api fetched')
         }).catch(err => {
             console.log(err)
         })
-    }, [state.id])
+    }, [id])
 
-    useEffect(async () => {
-        await getList()
-        await console.log(state)
+    const onSearch = (id)=> {
+        changeId(id)
+    }
+
+    useEffect(() => {
+        getList()
     }, [getList])
 
     return (
         <Layout style={{minHeight: '100vh'}}>
-            <div className="about">
+            <Space direction="vertical">
+                <Search placeholder="input search text" onSearch={onSearch} enterButton />
+            </Space>
 
-            </div>
+            <Table columns={columns} dataSource={data} />
         </Layout>
     );
 }
--- userConf/mockMap.js
+++ userConf/mockMap.js
@@ -3,13 +3,28 @@ export default {
         enable: true,
         data: {
             "code": 200,
-            "data|20": [
+            "data": [
                 {
-                    "username": "啊哈",
-                    "notificationType": 1,
-                    "age": 1,
-                    "address": "here"
-                }
+                    key: '1',
+                    name: 'John Brown',
+                    age: 32,
+                    address: 'New York No. 1 Lake Park',
+                    tags: ['nice', 'developer'],
+                },
+                {
+                    key: '2',
+                    name: 'Jim Green',
+                    age: 42,
+                    address: 'London No. 1 Lake Park',
+                    tags: ['loser'],
+                },
+                {
+                    key: '3',
+                    name: 'Joe Black',
+                    age: 32,
+                    address: 'Sidney No. 1 Lake Park',
+                    tags: ['cool', 'teacher'],
+                },
             ]
         }
     }
